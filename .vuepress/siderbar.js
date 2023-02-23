99c512cfa7777eef88b7f40cfe3474b6ea430c0a
J1aM1ng <2605379072@qq.com>
2023-02-24 01:57:29 +0800

    docs: 心得 all hands

--- .vuepress/siderbar.js
+++ .vuepress/siderbar.js
@@ -12,11 +12,11 @@ const OTHER_PATH = '/blog/other';
 module.exports = {
   [JAVASCRIPT_PATH]: [createSideBarConfig('JavaScript', JAVASCRIPT_PATH)],
   [CSS_PATH]: [createSideBarConfig('CSS', CSS_PATH)],
-  [PERFORM_PATH]: [createSideBarConfig('前端性能相关', PERFORM_PATH)],
+  [PERFORM_PATH]: [createSideBarConfig('behavior&mind', PERFORM_PATH)],
   [ALGORITHM_PATH]: [createSideBarConfig('算法', ALGORITHM_PATH)],
   [GIT_PATH]: [createSideBarConfig('Git', GIT_PATH)],
   [HTML_PATH]: [createSideBarConfig('HTML', HTML_PATH)],
   [GOLANG_PATH]: [createSideBarConfig('Golang', GOLANG_PATH)],
   [JAVA_PATH]: [createSideBarConfig('Java', JAVA_PATH)],
-  [OTHER_PATH]: [createSideBarConfig('工具', OTHER_PATH)],
+  [OTHER_PATH]: [createSideBarConfig('tools', OTHER_PATH)],
 };
